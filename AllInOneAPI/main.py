09e3a3a5cb2b0e431118a3f43a0a7d0dfcf6c047
fernando-cb12 <fernandocamoub@gmail.com>
2025-10-26 04:06:55 -0600

    fix cors

--- AllInOneAPI/main.py
+++ AllInOneAPI/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
